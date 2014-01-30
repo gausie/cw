80a302a4fa13e09082b5a31280352ecb73c99d2f
Gausie <git@gaus.ie>
2014-01-30 23:31:40 +0000

    Gave the div content and made it possible to close

--- cw.js
+++ cw.js
@@ -1,6 +1,6 @@
 (function($) {
 
-  $blackout = $('<div class="content-warning-blackout">content warning</div>');
+  $blackout = $('<aside class="content-warning-blackout"><h1>Content Warning</h1><p>This has been tagged as <span class="content-warning-reason"></span>.</p><button>Show</button></aside>');
 
   $(document).ready(function(){
 
@@ -9,6 +9,19 @@
       var $this = $(this);
 
       $fresh_bo = $blackout.clone();
+
+      var reason = $this.data('content-warning');
+      if(reason === "" || reason === "true"){
+        reason = "potentially triggering";
+      }
+
+      $('span.content-warning-reason', $fresh_bo).text(reason);
+
+      $('button', $fresh_bo).click(function(){
+        $fresh_bo.fadeOut(200, function(){
+          $fresh_bo.remove(); 
+        });
+      });
 
       if($this.css('position') == 'absolute'){
         console.log('abs');
